34179c262ac6d728b44f7bda686e80d8b6870baa
mohamedsaad308 <mohamedsaad308@gmail.com>
2020-10-23 20:37:37 +0200

    moving procifle to main folder

--- fyyur/config.py
+++ fyyur/config.py
@@ -7,7 +7,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 DEBUG = True
 ENV = "prod"
 # Connect to the database
-if ENV = "dev":
+if ENV == "dev":
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:2941@localhost:5432/fyyur'
 else:
     SQLALCHEMY_DATABASE_URI = "postgres://fqorpfkkidncet:9f756a8547563910c01518f6abc407a3ac33212dabe895e4ed9338bc8f5bb393@ec2-52-72-34-184.compute-1.amazonaws.com:5432/da80o58c4kqkvv"
